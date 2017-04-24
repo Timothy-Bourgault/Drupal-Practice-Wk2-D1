a7246baed327dc7e1d4db5436d22982765e3f2ad
Patrick McGreevy and Tim Bourgault <patrick+tim@>
2017-04-24 10:56:46 -0700

    Init commit

---
+++ array_practice.php
@@ -0,0 +1,9 @@
+<?php
+
+$clothes_to_buy = array("pants", "shirt", "hat", "shoes");
+$first = $clothes_to_buy[0];
+echo "<p> The first item is $first. </p>";
+$last = $clothes_to_buy[3];
+echo "<p> The last item is $last. </p>";
+
+?>
